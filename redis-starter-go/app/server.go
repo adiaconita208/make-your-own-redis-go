0464df216bbe57a587bba9230adac23c60a8ad3e
Adrian <adi2003diaconita@gmail.com>
2026-04-07 16:41:28 +0300

    feat: started working on lists chapter

--- redis-starter-go/app/server.go
+++ redis-starter-go/app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
@@ -18,10 +19,17 @@ type User struct {
 	Flags     []string
 }
 
+type LockableList struct {
+	sync.Mutex
+	elements []string
+}
+
 var ServerMemory sync.Map
 var UserRegistry sync.Map
+var ListRegistry sync.Map
 
 func main() {
+
 	listener, err := net.Listen("tcp", ":6379")
 
 	if err != nil {
@@ -53,7 +61,7 @@ func handleConnection(conn net.Conn) {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			log.Printf("Read error: %v", err)
-			return
+			continue
 		}
 
 		upperLine := strings.ToUpper(strings.TrimSpace(line))
@@ -76,6 +84,15 @@ func handleConnection(conn net.Conn) {
 
 		case strings.Contains(upperLine, "AUTH"):
 			handleAuth(reader, conn, &authUser)
+
+		case strings.Contains(upperLine, "RPUSH"):
+			handleRPush(reader, conn, &authUser)
+
+		case strings.Contains(upperLine, "LRANGE"):
+			handleLRange(reader, conn, &authUser)
+
+		case strings.Contains(upperLine, "LPUSH"):
+			handleLPush(reader, conn, &authUser)
 
 		}
 	}
@@ -109,10 +126,15 @@ func handleEcho(reader *bufio.Reader, conn net.Conn, authUser *string) {
 	content, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Read error: %v", err)
+		return
 	}
 	content = strings.TrimSpace(content)
 	response := fmt.Sprintf("$%d\r\n%s\r\n", len(content), content)
 	_, err = conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("Write error: %v", err)
+		return
+	}
 }
 
 func handleSet(reader *bufio.Reader, conn net.Conn, authUser *string) {
@@ -127,6 +149,7 @@ func handleSet(reader *bufio.Reader, conn net.Conn, authUser *string) {
 	key, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Error reading key: %v", err)
+		return
 	}
 	key = strings.TrimSpace(key)
 
@@ -134,6 +157,7 @@ func handleSet(reader *bufio.Reader, conn net.Conn, authUser *string) {
 	value, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Error reading value: %v", err)
+		return
 	}
 	value = strings.TrimSpace(value)
 	ServerMemory.Store(key, value)
@@ -145,6 +169,7 @@ func handleSet(reader *bufio.Reader, conn net.Conn, authUser *string) {
 		option, err := reader.ReadString('\n')
 		if err != nil {
 			log.Printf("Error reading the option: %v", err)
+			return
 		}
 		option = strings.ToUpper(strings.TrimSpace(option))
 
@@ -154,10 +179,13 @@ func handleSet(reader *bufio.Reader, conn net.Conn, authUser *string) {
 			tteNum, _ := strconv.Atoi(strings.TrimSpace(tte))
 			if err != nil {
 				log.Printf("Error reading time to expire: %v", err)
+				return
 			}
 
 			go func(tte int) {
-				time.Sleep(time.Millisecond * time.Duration(tte))
+				ctx, cancel := context.WithTimeout(context.Background(), time.Duration(tte)*time.Millisecond)
+				defer cancel()
+				<-ctx.Done()
 				ServerMemory.Delete(key)
 			}(tteNum)
 		}
@@ -176,16 +204,32 @@ func handleGet(reader *bufio.Reader, conn net.Conn, authUser *string) {
 	key, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Error reading key: %v", err)
+		return
 	}
 	key = strings.TrimSpace(key)
 	log.Print(key)
 	value, ok := ServerMemory.Load(key)
-	if !ok {
-		_, err = conn.Write([]byte("$-1\r\n"))
-	} else {
-		response := fmt.Sprintf("$%d\r\n%s\r\n", len(value.(string)), value.(string))
+
+	if ok {
+		stringValue, ok := value.(string)
+		if !ok {
+			log.Println("The value is not of type string")
+			return
+		}
+		response := fmt.Sprintf("$%d\r\n%s\r\n", len(stringValue), stringValue)
 		_, err = conn.Write([]byte(response))
+		if err != nil {
+			log.Printf("Writing Error: %v", err)
+		}
+		return
 	}
+
+	_, err = conn.Write([]byte("$-1\r\n"))
+	if err != nil {
+		log.Printf("Writing Error: %v", err)
+		return
+	}
+
 }
 
 func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
@@ -201,6 +245,7 @@ func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
 	cmd, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Error reading the ACL command: %v", err)
+		return
 	}
 	cmd = strings.ToUpper(strings.TrimSpace(cmd))
 
@@ -210,6 +255,7 @@ func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
 		_, err = conn.Write([]byte("$" + strconv.Itoa(len(*authUser)) + "\r\n" + *authUser + "\r\n"))
 		if err != nil {
 			log.Print("Writing error: ", err)
+			return
 		}
 
 	case "GETUSER":
@@ -222,6 +268,7 @@ func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
 		userInfo, ok := UserRegistry.Load(username)
 		if !ok {
 			log.Printf("User %s not found", username)
+			return
 		}
 		response := "*4\r\n$5\r\nflags\r\n*" + strconv.Itoa(len(userInfo.(*User).Flags)) + "\r\n"
 
@@ -242,6 +289,7 @@ func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
 		_, err = conn.Write([]byte(response))
 		if err != nil {
 			log.Print("Writing error: ", err)
+			return
 		}
 
 	case "SETUSER":
@@ -249,6 +297,7 @@ func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
 		username, err := reader.ReadString('\n')
 		if err != nil {
 			log.Printf("Error reading the username: %v", err)
+			return
 		}
 		username = strings.TrimSpace(username)
 
@@ -256,6 +305,7 @@ func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
 		arg, err := reader.ReadString('\n')
 		if err != nil {
 			log.Printf("Error reading the argument: %v", err)
+			return
 		}
 		if strings.HasPrefix(arg, ">") {
 			password, _ := strings.CutPrefix(arg, ">")
@@ -265,6 +315,7 @@ func handleACL(reader *bufio.Reader, conn net.Conn, authUser *string) {
 			val, ok := UserRegistry.Load(username)
 			if !ok {
 				log.Printf("User %s not found", username)
+				return
 			}
 
 			userInfo := val.(*User)
@@ -291,13 +342,15 @@ func handleAuth(reader *bufio.Reader, conn net.Conn, authUser *string) {
 	username, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Error reading the username: %v", err)
+		return
 	}
 	username = strings.TrimSpace(username)
 
 	_, _ = reader.ReadString('\n')
 	password, err := reader.ReadString('\n')
 	if err != nil {
-		log.Printf("Error reading the username: %v", err)
+		log.Printf("Error reading the password: %v", err)
+		return
 	}
 	password = strings.TrimSpace(password)
 
@@ -321,6 +374,168 @@ func handleAuth(reader *bufio.Reader, conn net.Conn, authUser *string) {
 		_, err = conn.Write([]byte("+OK\r\n"))
 	} else {
 		_, err = conn.Write([]byte("-WRONGPASS invalid username-password pair or user is disabled.\r\n"))
+	}
+}
+
+func handleRPush(reader *bufio.Reader, conn net.Conn, authUser *string) {
+	if !checkAuth(authUser) {
+		_, err := conn.Write([]byte("-NOAUTH Authentication required.\r\n"))
+		if err != nil {
+			log.Printf("Writing Error: %v", err)
+		}
+		return
+	}
+
+	_, _ = reader.ReadString('\n')
+	listKey, err := reader.ReadString('\n')
+	if err != nil {
+		log.Print("Error reading list key: ", err)
+		return
+	}
+	listKey = strings.TrimSpace(listKey)
+
+	var newElems []string
+	for reader.Buffered() > 0 {
+		_, _ = reader.ReadString('\n')
+		elem, err := reader.ReadString('\n')
+		if err != nil {
+			log.Print("Error reading element: ", err)
+			return
+		}
+		elem = strings.TrimSpace(elem)
+		newElems = append(newElems, elem)
+	}
+
+	listInterface, _ := ListRegistry.LoadOrStore(listKey, &LockableList{elements: []string{}})
+
+	list := listInterface.(*LockableList)
+
+	list.Lock()
+	defer list.Unlock()
+
+	list.elements = append(list.elements, newElems...)
+	response := fmt.Sprintf(":%d\r\n", len(list.elements))
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("Writing Error: %v", err)
+		return
+	}
+
+}
+
+func handleLRange(reader *bufio.Reader, conn net.Conn, authUser *string) {
+	if !checkAuth(authUser) {
+		_, err := conn.Write([]byte("-NOAUTH Authentication required.\r\n"))
+		if err != nil {
+			log.Printf("Writing Error: %v", err)
+		}
+		return
+	}
+
+	var response string
+
+	_, _ = reader.ReadString('\n')
+	listKey, err := reader.ReadString('\n')
+	if err != nil {
+		log.Print("Error reading list key: ", err)
+		return
+	}
+	listKey = strings.TrimSpace(listKey)
+
+	_, _ = reader.ReadString('\n')
+	index1Str, err := reader.ReadString('\n')
+	if err != nil {
+		log.Print("Error reading first index: ", err)
+		return
+	}
+	index1Str = strings.TrimSpace(index1Str)
+	index1, _ := strconv.Atoi(index1Str)
+
+	_, _ = reader.ReadString('\n')
+	index2Str, err := reader.ReadString('\n')
+	if err != nil {
+		log.Print("Error reading second index: ", err)
+		return
+	}
+	index2Str = strings.TrimSpace(index2Str)
+	index2, _ := strconv.Atoi(index2Str)
+
+	listInterface, ok := ListRegistry.Load(listKey)
+	if !ok {
+		response = "*0\r\n"
+	} else {
+		list := listInterface.(*LockableList)
+		list.Lock()
+		defer list.Unlock()
+
+		listLength := len(list.elements)
+
+		if index1 < 0 {
+			index1 = max(0, index1+listLength)
+		}
+
+		if index2 < 0 {
+			index2 = max(0, index2+listLength)
+		}
+
+		index1 = min(index1, listLength-1)
+		index2 = min(index2, listLength-1)
+
+		response = fmt.Sprintf("*%d\r\n", index2-index1+1)
+		for i := index1; i <= index2; i++ {
+			response += "$" + strconv.Itoa(len(list.elements[i])) + "\r\n" + list.elements[i] + "\r\n"
+		}
+	}
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Print("Error writing: ", err)
+		return
+	}
+}
+
+func handleLPush(reader *bufio.Reader, conn net.Conn, authUser *string) {
+	if !checkAuth(authUser) {
+		_, err := conn.Write([]byte("-NOAUTH Authentication required.\r\n"))
+		if err != nil {
+			log.Printf("Writing Error: %v", err)
+		}
+		return
+	}
+
+	_, _ = reader.ReadString('\n')
+	listKey, err := reader.ReadString('\n')
+	if err != nil {
+		log.Print("Error reading list key: ", err)
+		return
+	}
+	listKey = strings.TrimSpace(listKey)
+
+	var newElems []string
+	for reader.Buffered() > 0 {
+		_, _ = reader.ReadString('\n')
+		elem, err := reader.ReadString('\n')
+		if err != nil {
+			log.Print("Error reading element: ", err)
+			return
+		}
+		elem = strings.TrimSpace(elem)
+		newElems = append(newElems, elem)
+	}
+
+	listInterface, _ := ListRegistry.LoadOrStore(listKey, &LockableList{elements: []string{}})
+
+	list := listInterface.(*LockableList)
+
+	list.Lock()
+	defer list.Unlock()
+	for i := range newElems {
+		list.elements = append([]string{newElems[i]}, list.elements...)
+	}
+	response := fmt.Sprintf(":%d\r\n", len(list.elements))
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("Writing Error: %v", err)
+		return
 	}
 }
 
